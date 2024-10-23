38412879a99c9ec9d9339d57b498e5d4da71c7b9
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2024-10-23 10:07:33 +0000

    add port number to the script

--- jarm.sh
+++ jarm.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [ ! $# -eq 2 ]
+if [ ! $# -eq 3 ]
     then
-    echo "Two arguments required: (1) a list of IPs/domains in a"
-    echo "file, separated by line and (2) an output file name."
-    echo "Example: ./jarm.sh alexa500.txt jarm_alexa_500.csv"
+    echo "Three arguments required: (1) a list of IPs/domains in a"
+    echo "file, separated by line, (2) an output file name and, (3) the port number."
+    echo "Example: ./jarm.sh alexa500.txt jarm_alexa_500.csv 443"
     exit 1
  fi
 
 input=$1
+port=$3
 N=40
 (
 while IFS= read -r line
 do
   ((i=i%N)); ((i++==0)) && wait
-  python3 jarm.py $line -v -o $2 &
+  python3 jarm.py $line -p $port -v -o $2 &
 done < "$input"
 )
 wait
